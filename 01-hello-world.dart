3df523868b378af3ef9fb42d52900f133bbebe80
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2020-01-26 14:53:31 +0700

    Add more basic examples

--- 01-hello-world.dart
+++ 01-hello-world.dart
@@ -1,4 +1,18 @@
-void main(List<String> args) {
+/*main(List<String> args) {
+  print("Hello world");
+}*/
+
+/*void main(List<String> args) {
   var str = 'Hello world';
   print(str);
+}*/
+
+/*void main(List<String> args) {
+  int i = "Hello world";  // Wrong type assignment here
+  print(i);
+}*/
+
+void main(List<String> args) {
+  String $result = "Hello World";
+  print($result)
 }
