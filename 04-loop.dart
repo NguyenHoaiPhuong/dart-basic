c757a5ba4e4f8523932b2bbabb8221fc914becb3
Nguyen Hoai Phuong <phuong.nguyen@cel-consulting.com>
2020-02-07 13:19:07 +0700

    [loop] for loop

---
+++ 04-loop.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+int factorial(int input) {
+  if (input < 0) {
+    print("Cannot calculate the factorial of a negative number: ${input}");
+    return -1;
+  }
+  if (input == 0) {
+    return 1;
+  }
+
+  int output = 1;
+  for (int i = 1; i < input + 1; i++) {
+    output *= i;
+  }
+
+  return output;
+}
+
+void main(List<String> args) {
+  print("Input an integer number:");
+  var rawString = stdin.readLineSync();
+  int input = int.parse(rawString);
+  int output = factorial(input);
+  print("Factorial of ${input} is ${output}");
+}
