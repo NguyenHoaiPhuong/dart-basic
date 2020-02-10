c05de322f8b706bc0ed7c1aa1cbee999cbeced28
Nguyen Hoai Phuong <phuong.nguyen@cel-consulting.com>
2020-02-10 12:36:36 +0700

    boolean

---
+++ 08-boolean.dart
@@ -0,0 +1,13 @@
+void TestBoolean() {
+  print("Test boolean...");
+
+  var a = 7, b = 12;
+  bool res = a > b;
+  print("Is ${a} > ${b}? ${res}");
+
+  print("-----------------------------");
+}
+
+void main(List<String> args) {
+  TestBoolean();
+}
