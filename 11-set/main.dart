e5beb9fe63f481616f87ba4bcf6cc940869fa374
Nguyen Hoai Phuong <phuong.nguyen@cel-consulting.com>
2020-02-12 10:54:40 +0700

    set

---
+++ 11-set/main.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  // set includes distinct values
+  // number 1 is declared 2 times, but it is stored only 1 time in the set
+  Set aSet = {1, "yushin", 'akagi', 2.5, 1};
+  print(aSet);
+}
